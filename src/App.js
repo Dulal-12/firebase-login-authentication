1dc50e813f1c1d446aadb1b8fdb5aada7ce05b75
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-01-05 19:58:28 +0600

    done firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import firebase from 'firebase/app'
+import "firebase/auth";
+import  firebaseConfig from './firebaseConfig';
+firebase.initializeApp(firebaseConfig);
 function App() {
+
+  const [user , setUser] = useState({
+    isSignIn : false,
+    name : '',
+    photo : ''
+  });
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = ()=>{
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result=>{
+      const {displayName , photoURL} = result.user;
+      const signedIn = { isSignIn  :true , name : displayName , photo : photoURL};
+      setUser(signedIn);
+     // console.log(email , displayName , photoURL);
+    })
+    .catch(err=>console.log(err.message))
+  }
+  const handleSignOut = ()=>{
+
+    firebase.auth().signOut()
+    .then(res=>{
+      const signOut =   { isSignIn : false,
+        name : '',
+        photo : ''}
+        setUser(signOut);
+    })
+   
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {(user.isSignIn) ?  <button onClick={handleSignOut}>Sign Out</button>   :<button onClick={handleSignIn}>Sign In</button>}
+     {
+       (user.isSignIn)?<div>
+         <p>{user.name}</p>
+         <img src={user.photo} alt="" srcset=""/>
+        
+       </div>: null
+     }
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyB60ApnQzN62Y1BQV4suzp3iK5XIjh-W1k",
+    authDomain: "dlaik-3b812.firebaseapp.com",
+    databaseURL: "https://dlaik-3b812.firebaseio.com",
+    projectId: "dlaik-3b812",
+    storageBucket: "dlaik-3b812.appspot.com",
+    messagingSenderId: "350923670486",
+    appId: "1:350923670486:web:e8fcf49f58d2c9caad1a77"
+  };
+  export default firebaseConfig;
